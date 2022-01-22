e8ea61b73c82cbf452f172d205d687ded4a5a4b8
Nick Zahnow <nzahnow@gmail.com>
2022-01-21 20:35:11 -0600

    completed POST for todos

--- server/routes/todosRouter.js
+++ server/routes/todosRouter.js
@@ -5,7 +5,7 @@ const pool = require('../modules/pool');
 //TODO
 
 router.get('/', (req, res) => {
-    console.log('GET request on /todo');
+    console.log('GET request on /todos');
     const queryString = `SELECT * FROM "todos"`;
     pool.query(queryString)
         .then((response) => {
@@ -13,6 +13,22 @@ router.get('/', (req, res) => {
         })
         .catch((error) => {
             console.log('Error getting todos:', error);
+            res.sendStatus(500);
+        });
+});
+
+router.post('/', (req, res) => {
+    console.log('POST request on /todos');
+    const queryString = `
+        INSERT INTO "todos" ("summary")
+        VALUES ($1);
+    `
+    pool.query(queryString, [req.body.summary])
+        .then((response) => {
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log('Error POSTing to todos:', error);
             res.sendStatus(500);
         });
 });
