3c2352dc8e33854479486ae8496313919350bf58
Nick Zahnow <nzahnow@gmail.com>
2022-01-21 21:19:40 -0600

    delete in working in client

--- server/public/scripts/script.js
+++ server/public/scripts/script.js
@@ -5,7 +5,8 @@ function onReady(){
 
     // Set up click handlers
     $('#add-to-do').on('click', addTodo);
-
+    $('#todos-table').on('click', '.delete-button', deleteTodo);
+    $('#todos-table').on('click', '.complete-button', completeTodo);
     // Get todo list from server
     refreshTodos();
 }
@@ -26,6 +27,23 @@ function addTodo() {
 
 }
 
+function deleteTodo(event) {
+    const id = $(event.target).data('id');
+    console.log('clicked delete on id:', id);
+     $.ajax({
+         method: 'DELETE',
+         url: `/todos/${id}`
+    }).then((response) => {
+        refreshTodos();
+    }).catch((error) => {
+        console.log('Error deleting todo:', error);
+    })
+}
+
+function completeTodo(event) {
+    console.log('clicked complete');
+}
+
 function refreshTodos() {
     $('#todos-table').empty();
     $.ajax({
@@ -35,9 +53,9 @@ function refreshTodos() {
         for(let item of response) {
             $('#todos-table').append(`
                 <tr>
-                    <td><button>Complete</button></td>
+                    <td><button class='complete-button'>Complete</button></td>
                     <td>${item.summary}</td>
-                    <td><button>Delete</button></td>
+                    <td><button class='delete-button' data-id="${item.id}">Delete</button></td>
                 </tr>
             `)
         }
