7dc805078e2f5c0e2a3a24e2d1448dffeefdbc22
Nick Zahnow <nzahnow@gmail.com>
2022-01-24 15:56:46 -0600

    replacing the pool.js file with the example from notes

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,25 +1,42 @@
 const pg = require('pg');
+const url = require('url');
 
-// Setup PG to connect to the database
-const Pool = pg.Pool;
-const pool = new Pool({
-    database: process.env.DATABASE_NAME || 'weekend_todos', // the name of database, This can change!
-    host: 'localhost', // where is your database?
-    port: 5432, // the port for your database, 5432 is default for postgres
-    max: 10, // how many connections (queries) at one time
-    idleTimeoutMillis: 30000 // 30 second to try to connect, otherwise cancel query
-});
+let config = {};
 
-// .on here looks familiar...this is how node can handle arbitrary events
-// this is NOT required but it is very useful for debugging
+// We need a different pg configuration if we're running
+// on Heroku, vs if we're running locally.
+//
+// Heroku gives us a process.env.DATABASE_URL variable,
+// so if that's set, we know we're on heroku.
+if (process.env.DATABASE_URL) {
+  config = {
+    // We use the DATABASE_URL from Heroku to connect to our DB
+    connectionString: process.env.DATABASE_URL,
+    // Heroku also requires this special `ssl` config
+    ssl: { rejectUnauthorized: false },
+  };
+} else {
+  // If we're not on heroku, configure PG to use our local database
+  config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'weekend_todos', // CHANGE THIS LINE to match your local database name!
+  };
+}
+
+// this creates the pool that will be shared by all other modules
+const pool = new pg.Pool(config);
+
+// the pool will log when it connects to the database
 pool.on('connect', () => {
-    console.log('Postgresql connected');
+  console.log('Postgesql connected');
 });
 
 // the pool with emit an error on behalf of any idle clients
-// it contains if a back end error or network partition happens
-pool.on('error', (error) => {
-    console.log('Error with postgres pool', error)
+// it contains if a backend error or network partition happens
+pool.on('error', (err) => {
+  console.log('Unexpected error on idle client', err);
+  process.exit(-1);
 });
 
 module.exports = pool;
